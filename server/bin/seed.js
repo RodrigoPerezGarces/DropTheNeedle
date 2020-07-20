58fa7604a5079dfae720f37aa2b5745ecd82fc41
Rodrigo <user@Rodrigos-MBP.home>
2020-07-20 16:54:08 +0200

    first commit

---
+++ server/bin/seed.js
@@ -0,0 +1,57 @@
+// Database
+const mongoose = require('mongoose')
+const dbName = 'drop-the-needle'
+mongoose.connect(`mongodb://localhost/${dbName}`)
+
+// Model
+
+const Disk = require('../models/disk.model')
+
+// DATA
+
+const vinyls = [
+    {
+        title: ,
+        image: ,
+        artists: ,
+        label: ,
+        genre: ,
+        price: ,
+        year: ,
+        CatNo: ,
+
+    },
+    {
+        title: ,
+        image: ,
+        artists: ,
+        label: ,
+        genre: ,
+        price: ,
+        year: ,
+        CatNo: ,
+
+    },
+    {
+        title: ,
+        image: ,
+        artists: ,
+        label: ,
+        genre: ,
+        price: ,
+        year: ,
+        CatNo: ,
+
+    },
+    {
+        title: ,
+        image: ,
+        artists: ,
+        label: ,
+        genre: ,
+        price: ,
+        year: ,
+        CatNo: ,
+
+    }
+]

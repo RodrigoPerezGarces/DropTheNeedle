c1b374f3e5c7bcd8f4a5a92529f9f7e273c25c52
Rodrigo <user@Rodrigos-MBP.home>
2020-07-24 18:03:11 +0200

    deploy3

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ module.exports = app => {
     // Base URLS
     app.use('/api/vinyls', require('./vinyl.routes'))
     app.use('/api', require('./auth.routes'))
-    // app.use((req, res) => {
-    //     res.sendFile(__dirname + "/public/index.html");
-    // })
+    app.use((req, res) => {
+        res.sendFile(__dirname + "/public/index.html");
+    })
 }
